3f9e45c80a176a6c54a2936508369e75199ca3c7
Marius Pirvu <mpirvu@ca.ibm.com>
2023-10-23 10:34:46 -0400

    Improvements to runDaCopo.py script
    Automatically destroy SCC when specified on the command line and using cold runs.
    Allow computing the average of the last N iterations.
    
    Signed-off-by: Marius Pirvu <mpirvu@ca.ibm.com>

--- runDaCapo.py
+++ runDaCapo.py
@@ -5,7 +5,7 @@
 # The script can be customized as follows:
 # 1. Specify which benchmarks to run ==> change "benchmark" list below
 # 2. Specify the number iterations for each benchmark (to warm up the JVM) ==> change "benchmarkOpts" below
-# 3. Specify the number of runs for each benchmark ==> change "numIter" below
+# 3. Specify the number of runs for each benchmark ==> change "numRuns" below
 # 4. Specify JDK options ==> change "jvmOption" list below
 # 5. Specify JDK to use ==> change "jdks" list below
 
@@ -16,14 +16,15 @@ import logging
 import numpy as np
 
 
-numIter = 10 # number of iterations to use for each benchmark in each configuration
-
+numRuns = 100 # number of runs to use for each benchmark in each configuration
+benchmarkOpts = "--iterations 1 -s default" # not all benchmarks can use size large. Better to use "default"
+numlastIterForComputingAvg = 1 # number of last iterations for each JVM used for computing the average execution time
+                                # Must be smaller that --iterations in benchmarkOpts
+doColdRun = True # If True, destroy the SCC before each benchmark
+affinity = "taskset 0x3"
 #level=logging.DEBUG,
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s :: %(levelname)s :: (%(threadName)-6s) :: %(message)s',
-                    )
+logging.basicConfig(level=logging.INFO, format='%(asctime)s :: %(levelname)s :: (%(threadName)-6s) :: %(message)s',)
 
-benchmarkOpts = "--iterations 20 -s default" # not all benchmarks can use size large. Better to use "default"
 
 # List of benchmarks to run
 # avrora batik eclipse fop h2 jython luindex lusearch lusearch-fix pmd sunflow tomcat tradebeans tradesoap xalan
@@ -31,9 +32,9 @@ benchmarks = [
     #"avrora",
     #"batik", Not working at all with OpenJDK
     #"eclipse", # only working with Java8
-    #"fop",
+    "fop",
     #"h2",
-    #"jython",
+    #"jython", # Does not run Java17
     #"luindex",
     #"lusearch-fix",
     #"pmd",
@@ -41,48 +42,83 @@ benchmarks = [
     #"tomcat", # does not work at all
     #"tradebeans", # does not work at all
     #"tradesoap", # does not work at all
-    "xalan"
+    #"xalan"
 ]
 
 # List of JVM options to try
 jvmOptions = [
-    #"-Xmx1G",
-    #"-XX:-EnableHCR -Xmx1G",
-    "-XX:-OSRSafePoint -Xmx1G",
-    #"-Xaggressive -Xmx1G",
-    #"-Xms1G -Xmx1G",
-    #"-Xtune:throughput -Xmx1G",
-    #"-Xtune:throughput -Xmx1G -Xjit:acceptHugeMethods",
-    #"-Xtune:throughput -Xmx1G -Xjit:inlineVeryLargeCompiledMethods",
-    #"-Xtune:throughput -Xmx1G -Xjit:bigCalleeFreqCutoffAtHot=0",
-    #"-Xtune:throughput -Xmx1G -Xjit:bigCalleeThresholdForColdCallsAtHot=600",
+    #"",
+    "-Xms1G -Xmx1G",
 ]
 
 jdks = [
-    #"/home/mpirvu/FullJava11/openj9-openjdk-jdk11/build/linux-x86_64-normal-server-release/images/jdk",
-    #"/home/mpirvu/FullVM/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/images/j2re-image",
-    "/home/mpirvu/sdks/OpenJ9-JDK8-x86-64_linux-20220126-021513",
+    #"/home/mpirvu/sdks/OpenJDK17U-jre_x64_linux_hotspot_17.0.8.1_1",
+    "/home/mpirvu/FullJava17/openj9-openjdk-jdk17/build/linux-x86_64-server-release/images/jdk"
 ]
+
+def destroySCC(jvm, jvmOpts):
+    print(jvm, jvmOpts)
+    cmd = ""
+    # Parse the options and try to figure out the location of the SCC
+    # This should be something like -Xshareclasses:cacheDir=<name>
+    # match alphanum and _ and . and / but not comma
+    m = re.search('cacheDir=([a-zA-Z0-9_\./]+)', jvmOpts)
+    if m:
+        cacheDir = m.group(1)
+        cmd = f"{jvm}/bin/java -Xshareclasses:cacheDir={cacheDir},destroyall"
+    else:
+        cmd = f"{jvm}/bin/java -Xshareclasses:destroyall"
+    #logging.info("Destroying SCC with: {cmd}".format(cmd=cmd))
+    try:
+        output = subprocess.check_output(shlex.split(cmd), universal_newlines=True, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        # If the SCC does not exist, we get a non-zero return code
+        output = e.output
+    except subprocess.SubprocessError as e:
+        logging.warning("SubprocessError clearing SCC: {e}".format(e=e))
+        output = str(e)
+    logging.info("{output}".format(output=output))
+    return 0
 
 '''
 Returns the execution time in milliseconds as a float
 or Nan if the experiment fails
 '''
-def runBenchmarkOnce(benchmarkName, jvm, jvmOpts):
-    cmd = f"{jvm}/bin/java {jvmOpts} -jar dacapo-9.12-MR1-bach.jar {benchmarkOpts} {benchmarkName}"
+def runBenchmarkOnce(benchmarkName, jvm, jvmOpts, benchIter):
+    cmd = f"{affinity} {jvm}/bin/java {jvmOpts} -jar dacapo-9.12-MR1-bach.jar {benchmarkOpts} {benchmarkName}"
     logging.info("Starting: {cmd}".format(cmd=cmd))
     output = subprocess.check_output(shlex.split(cmd), universal_newlines=True, stderr=subprocess.STDOUT)
-    # Parse the output and look for "PASSED in nnnn msec ====
+    # Parse the output and look for "PASSED in nnnn msec" or "completed warmup nnn in nnnn msec" ====
+    #===== DaCapo 9.12-MR1 fop starting warmup 1 =====
+    #===== DaCapo 9.12-MR1 fop completed warmup 1 in 1672 msec =====
+    #...
+    #===== DaCapo 9.12-MR1 fop starting =====
+    #===== DaCapo 9.12-MR1 fop PASSED in 242 msec =====
+
+
     lines = output.splitlines()
-    pattern = re.compile('^===== DaCapo .+ PASSED in (\d+) msec ====')
+    pattern1 = re.compile('^===== DaCapo .+ PASSED in (\d+) msec ====')
+    pattern2 = re.compile('^===== DaCapo .+ completed warmup \d+ in (\d+) msec ====')
+    foundPassed = False
+    runTimes = np.full(benchIter, fill_value=np.nan, dtype=float)
+    i = 0
     for line in lines:
-        m = pattern.match(line)
+        #print(line)
+        m = pattern1.match(line)
         if m:
-            print(line)
-            execTime = float(m.group(1))
-            print(execTime)
-            return execTime
-    return np.nan
+            runTimes[i] = float(m.group(1))
+            foundPassed = True
+            i = i + 1
+        else:
+            m = pattern2.match(line)
+            if m:
+                runTimes[i] = float(m.group(1))
+                i = i + 1
+    # Compute the average time of the last N iterations
+    avgTime = np.nanmean(runTimes[-numlastIterForComputingAvg:])
+    print(avgTime)
+
+    return avgTime if foundPassed else np.nan
     #print(output)
 
 def tdistribution(degreesOfFreedom):
@@ -114,13 +150,25 @@ def tdistribution(degreesOfFreedom):
 #    results = st.t.interval(0.95, len(a)-1, loc=0, scale=st.sem(a))
 #    return 100.0 * results[1] / st.tmean(a)
 
+# Determine the number of iterations to use for each benchmark
+m = re.compile('--iterations (\d+)').match(benchmarkOpts)
+benchIter = int(m.group(1)) if m else sys.exit('Cannot determine number of iterations from benchmarkOpts')
+if benchIter < numlastIterForComputingAvg:
+    sys.exit('Number of iterations for computing the average must be smaller than the total number of iterations')
+
+if doColdRun:
+    print("Will do cold")
+
 # multi-dimensional array of results
-results = np.full((len(benchmarks), len(jdks), len(jvmOptions), numIter), fill_value=np.nan, dtype=np.float)
+results = np.full((len(benchmarks), len(jdks), len(jvmOptions), numRuns), fill_value=np.nan, dtype=float)
 for bench in range(len(benchmarks)):
     for jdk in range(len(jdks)):
         for opt in range(len(jvmOptions)):
-            for i in range(numIter):
-                execTime = runBenchmarkOnce(benchmarks[bench], jdks[jdk], jvmOptions[opt])
+            if doColdRun:
+                destroySCC(jdks[jdk],jvmOptions[opt])
+                runBenchmarkOnce(benchmarks[bench], jdks[jdk], jvmOptions[opt], benchIter) # discard the cold run
+            for i in range(numRuns):
+                execTime = runBenchmarkOnce(benchmarks[bench], jdks[jdk], jvmOptions[opt], benchIter)
                 results[bench, jdk, opt, i] = execTime
 
 # Stats ignoring Nan which are due to failed experiments
@@ -131,7 +179,7 @@ max  = np.nanmax(results, axis=3)
 
 # Count valid experiments excluding Nan values
 numValidExperiments = np.count_nonzero(~np.isnan(results), axis=3)
-print(numValidExperiments)
+#print(numValidExperiments)
 
 # Create my function that will apply "tdistribution" to all elements in an ndarray
 tdist_vec = np.vectorize(tdistribution)
